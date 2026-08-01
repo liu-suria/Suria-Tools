6ff6afdfee5247115860d33a51fa4f008f5eb90c
liu-suria <liu@shual.cn>
2026-08-01 17:23:27 +0800

    更新稳定版离线缓存

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='suria-tools-final-20260801-3';
-const ASSETS=['./','index.html','final.css','polish.css','final.js','tools-extra.js','polish.js','manifest.webmanifest','icon.svg'];
+const CACHE='suria-tools-final-20260801-4';
+const ASSETS=['./','index.html','final.css','polish.css','bootstrap.js','final.js','tools-extra.js','polish.js','stability.js','manifest.webmanifest','icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
